9a7a203262ee3c4163bf92e7ee722de36e6e4c81
Kobell <k4kobell@gmail.com>
2024-09-29 21:52:56 -0500

    commit_make_charts

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -13,6 +13,18 @@ def hello(user=None):
 def welcome():
     return render_template('welcome.html')
 
-@app.route('/draw')
-def draw():
-    return f'<!doctype html><body><div>{fr.make_chart()}</div></body>'
+@app.route('/draw_price')
+def draw_price_line_graph():
+    return f'<!doctype html><body><div style="text-align:right;">{fr.make_price_svg_chart()}</div></body>'
+
+@app.route('/draw_volume')
+def draw_volume_bar_graph():
+    return f'<!doctype html><body><div style="text-align:right;">{fr.make_volume_svg_chart()}</div></body>'
+
+@app.route('/draw_two')
+def draw_two_share_xaxis():
+    return f'<!doctype html><body><div style="text-align:center;">{fr.make_two_svg_chart()}</div></body>'
+
+@app.route('/draw_xaxis')
+def draw_xaxis_only():
+    return f'<!doctype html><body><div style="text-align:right;">{fr.make_xaxis_only_svg_chart()}</div></body>'
